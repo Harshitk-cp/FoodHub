9b904606ae612ff8560c993516895fd44f682941
Harshitk-cp <harshitk.cp@gmail.com>
2020-12-23 02:17:55 +0530

    Initial commit

---
+++ app/src/main/java/com/harshit/foodhub/activity/ProfileActivity.kt
@@ -0,0 +1,13 @@
+package com.harshit.foodhub.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.harshit.foodhub.R
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
